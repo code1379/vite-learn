839f89b9466692a4e871c99109c3d1a8001e7011
thiem <coderxialuo@gmail.com>
2022-07-20 21:58:31 +0800

    feat: css 使用 alias，预处理不支持 alias 写法

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { defineComponent } from "vue";
+// JSX 这样引入可以，但在 vue-template 的 scope中 @import url("./styles/app.css") 则无效，需要在 main.js 中引入
+import "@styles/app.css";
+import "@styles/test.less";
+import classes from "@styles/test.module.css";
 
 export default defineComponent({
   setup() {
     return () => {
-      return <>Hello vue jsx</>;
+      return (
+        <div className="container">
+          <h1>Hello vue jsx</h1>
+          <p className={classes.moduleClass}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+      );
     };
   },
 });
